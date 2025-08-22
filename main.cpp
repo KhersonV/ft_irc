9bb3cabbd9eb6f6b50751bce91a65fa64a921b47
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-08-22 12:47:01 +0200

    listening + testing + refactor

--- main.cpp
+++ main.cpp
@@ -17,28 +17,17 @@ int set_nonblocking(int fd) {
 	return 0;
 }
 
-int main(void) {
-
+int create_listen_socket(int port) {
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (fd < 0) {
 		std::perror("socket");
 		return -1;
 	}
-
 	int yes = 1;
 	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
 		std::perror("setsockupt SO_REUSEADDR");
 		close(fd);
 		return -1;
-	}
-
-	// SO_REUSEADDR тест, удалить
-	int val = 0;
-	socklen_t len = sizeof(val);
-	if (getsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, &len) == -1) {
-		std::perror("gertsockopt SO_REUSEADDR");
-	} else {
-		std::cout << "SO_REUSEADDR: " << val << "\n";
 	}
 
 	if (set_nonblocking(fd) < 0) {
@@ -47,26 +36,15 @@ int main(void) {
 		return -1;
 	}
 
-	// Non-Block тест, удалить
-	int f2 = fcntl(fd, F_GETFL, 0);
-	if (f2 == -1) {
-		std::perror("fcntl F_GETFL (after)");
-	} else {
-	 std::cout << "nonblock: " << ((f2 & O_NONBLOCK) ? "ON" : "OFF") << "\n";
-	}
-
-
-
 	sockaddr_in address;
-	int port = 6667;
 	memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET; //ipv4
+	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(port);
-
 	// при addr.sin_port = htons(0); ядро само выбирает порт
 	// getdockname(fd, (sockaddr*)&address, &len) == 0)
 	// std::cout << "bound to port " << ntohs(got.sin_port) << "\n";
+
 
 	if (bind(fd, (sockaddr*)&address, sizeof(address)) < 0) {
 		std::perror("bind");
@@ -74,8 +52,23 @@ int main(void) {
 		return -1;
 	}
 
+	int backlog = 128;
 
-	close(fd);
+	if(listen(fd, backlog) < 0) {
+		std::perror("listen");
+		close(fd);
+		return -1;
+	}
+	std::cout << "Listening...\n";
+	return fd;
+}
 
+int main(void) {
+	int port = 6667;
+	int server_fd = create_listen_socket(port);
+	if (server_fd < 0)
+		return 1;
+	std::cin.get();
+	close(server_fd);
 	return 0;
 }
