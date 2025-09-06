195e68e9af857c403620c98dc992313e58a6af45
textualbois <wivna360@hotmail.com>
2025-09-06 16:25:00 +0200

    password reply codes

--- src/Cmd_pass.cpp
+++ src/Cmd_pass.cpp
@@ -1,15 +1,18 @@
 #include "Cmd_core.hpp"
 #include "Proto.hpp"
 #include "State.hpp"
+#include "Irc_codes.hpp"
+
+using namespace ft_codes;
 
 bool handle_PASS(int fd, Client &cl, std::map<int, Client> &clients, const std::string &rest)
 {
 	if (cl.registered) {
-		send_numeric(clients, fd, 462, cl.nick, "", "You may not reregister");
+		send_numeric(clients, fd, ALREADY_REGISTERED, cl.nick, "", "Already registered");
 		return false;
 	}
 	if (rest.empty()) {
-		send_numeric(clients, fd, 461, cl.nick, "PASS", "Not enough parameters");
+		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "PASS", "Not enough parameters");
 		return false;
 	}
 
@@ -19,7 +22,7 @@ bool handle_PASS(int fd, Client &cl, std::map<int, Client> &clients, const std::
 	if (!pass.empty() && pass[0] == ':') pass.erase(0, 1);
 
 	if (!g_state.server_password.empty() && pass != g_state.server_password) {
-		send_numeric(clients, fd, 464, cl.nick, "", "Password incorrect");
+		send_numeric(clients, fd, PASSWORD_MISMATCH, cl.nick, "", "Password incorrect");
 		return false;
 	}
 
