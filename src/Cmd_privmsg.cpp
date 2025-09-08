cb18e5eeeb39ba20c87b561709ff05177ccef66e
textualbois <wivna360@hotmail.com>
2025-09-08 19:24:30 +0200

    privmsg extracted

---
+++ src/Cmd_privmsg.cpp
@@ -0,0 +1,120 @@
+#include "Cmd_core.hpp"
+
+using namespace ftirc;
+using namespace ft_codes;
+
+namespace
+{
+bool	is_registered(Client &cl, int fd, std::map<int, Client> &clients)
+{
+	if (!cl.registered)
+	{
+		send_numeric(clients, fd, NOT_REGISTERED,
+			cl.nick.empty() ? "*" : cl.nick, "", "You have not registered, register with NICK and USER");
+		return (false);
+	}
+	return (true);
+}
+
+bool	find_channel(const std::string &chname, Channel *&out_ch,
+		const Client &cl, int fd, std::map<int, Client> &clients)
+{
+	std::map<std::string,
+		Channel>::iterator it = g_state.channels.find(lower_str(chname));
+	if (it == g_state.channels.end())
+	{
+		send_numeric(clients, fd, NO_SUCH_CHANNEL, cl.nick, chname,
+			"No such channel");
+		return (false);
+	}
+	out_ch = &it->second;
+	return (true);
+}
+
+bool parse_privmsg(const std::string &rest, std::string &target, std::string &text, const Client &cl, int fd, std::map<int, Client> &clients)
+{
+	if (rest.empty()) {
+		send_numeric(clients, fd, NO_RECIPIENT, cl.nick, "PRIVMSG",
+					"No recipient given (PRIVMSG)");
+		return false;
+	}
+
+	target = rest;
+	std::string::size_type sp = target.find(' ');
+	if (sp != std::string::npos) target.erase(sp);
+
+	if (target.empty()) {
+		send_numeric(clients, fd, NO_RECIPIENT, cl.nick, "PRIVMSG",
+					"No recipient given (PRIVMSG)");
+		return false;
+	}
+
+	std::string::size_type pos_trailing = rest.find(" :");
+	if (pos_trailing == std::string::npos) {
+		send_numeric(clients, fd, NO_TEXT_TO_SEND, cl.nick, "", "No text to send");
+		return false;
+	}
+
+	text = rest.substr(pos_trailing + 2);
+	return true;
+}
+
+bool send_privmsg_to_channel(std::map<int, Client> &clients, int fd, const Client &cl, const std::string &target_chan, const std::string &text)
+{
+	Channel *ch = 0;
+	if (!find_channel(target_chan, ch, cl, fd, clients)) {
+		return (false);
+	}
+
+	if (!is_member(*ch, fd)) {
+		send_numeric(clients, fd, CANNOT_SEND_TO_CHAN, cl.nick, target_chan,
+					"cannot send to channel");
+		return false;
+	}
+
+	const std::string line = ":" + cl.nick + " PRIVMSG " + target_chan + " :" + text;
+	send_to_channel(clients, *ch, line, fd);
+	return true;
+}
+
+bool find_target_fd_by_nick(const std::string &nick, int &out_fd, const Client &cl, int fd, std::map<int, Client> &clients)
+{
+	std::map<std::string,int>::iterator it = g_state.nick2fd.find(lower_str(nick));
+	if (it == g_state.nick2fd.end()) {
+		send_numeric(clients, fd, NO_SUCH_NICK, cl.nick, nick, "No such nick");
+		return false;
+	}
+	out_fd = it->second;
+	return true;
+}
+
+inline void send_privmsg_to_user(std::map<int, Client> &clients, int rfd, const Client &cl, const std::string &target_nick, const std::string &text)
+{
+	const std::string from = cl.nick.empty() ? "*" : cl.nick;
+	const std::string line = ":" + from + " PRIVMSG " + target_nick + " :" + text;
+	enqueue_line(clients, rfd, line);
+}
+
+} // anonymous namespace
+
+bool handle_PRIVMSG(int fd, Client &cl, std::map<int, Client> &clients, const std::string &rest)
+{
+	if (!is_registered(cl, fd, clients))
+		return false;
+
+	std::string target, text;
+	if (!parse_privmsg(rest, target, text, cl, fd, clients))
+		return false;
+
+	if (!target.empty() && target[0] == '#') {
+		send_privmsg_to_channel(clients, fd, cl, target, text);
+		return false;
+	}
+
+	int rfd = -1;
+	if (!find_target_fd_by_nick(target, rfd, cl, fd, clients))
+		return false;
+
+	send_privmsg_to_user(clients, rfd, cl, target, text);
+	return false;
+}
