93102cd7f035d5d4308827e231920df7e8863134
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-09-21 15:33:26 +0200

    parse_args helper + member helper added

--- src/Cmd_kick.cpp
+++ src/Cmd_kick.cpp
@@ -31,6 +31,44 @@ namespace {
 		return true;
 	}
 
+	bool parse_kick_args(const std::string &rest,
+						std::string &chname,
+						std::string &target_nick,
+						std::string &reason,
+						const Client &cl, int fd, std::map<int, Client> &clients)
+	{
+		if (rest.empty()) {
+					send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "KICK",
+					"Usage: KICK #channel <nick> [:reason]");
+		return false;
+		}
+
+		std::string rest2 = ltrim(rest);
+		chname = first_token(rest2);
+		if (chname.empty() || chname[0] != '#')
+		{
+			send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "KICK",
+						"Usage: KICK #channel <nick> [:reason]");
+			return false;
+		}
+
+		std::string after = ltrim(rest2.substr(chname.size()));
+		target_nick = first_token(after);
+		if (target_nick.empty()) {
+					send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "KICK",
+					"Not enough parameters");
+			return false;
+		}
+
+		std::string::size_type pos_trailing = after.find(" :");
+		if (pos_trailing != std::string::npos)
+			reason = after.substr(pos_trailing + 2);
+		else
+			reason.clear();
+
+		return true;
+	}
+
 	bool	lookup_target_fd(const std::string &target, int &out_fd,
 		const Client &cl, int fd, std::map<int, Client> &clients)
 	{
@@ -58,6 +96,23 @@ namespace {
 		return (true);
 	}
 
+	bool target_must_be_member(const Channel &ch, const std::string &chname,
+							const std::string &target_nick, int target_fd,
+							const Client &cl, int fd,
+							std::map<int, Client> &clients)
+	{
+		if(!is_member(ch, target_fd))
+		{
+			send_numeric(clients, fd, USER_NOT_IN_CHANNEL, cl.nick,
+						target_nick + " " + chname,
+						"They aren't on that channel");
+		return false;
+		}
+		return true;
+	}
+
+
+
 	bool	must_be_op(const Channel &ch, int fd, std::map<int, Client> &clients,
 			const Client &cl)
 	{
@@ -73,7 +128,13 @@ namespace {
 	bool handle_KICK(int fd, Client &cl, std::map<int, Client> &clients,
 		const std::string &rest)
 	{
-		return 1;
+		if (!is_registered(cl, fd, clients))
+			return false;
+
+		std::string chname, target_nick, reason;
+
+		if (!parse_kick_args(rest, chname, target_nick, reason, cl, fd, clients))
+			return false;
 
 	}
 }
