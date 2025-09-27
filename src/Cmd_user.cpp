328c957bdd70e11e645f678191c029f30c6f5907
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-09-27 22:07:28 +0200

    user command protection added

--- src/Cmd_user.cpp
+++ src/Cmd_user.cpp
@@ -3,6 +3,7 @@
 #include "Irc_codes.hpp"
 
 using namespace ft_codes;
+using namespace	ftirc;
 
 namespace
 {
@@ -16,33 +17,67 @@ bool is_register_possible(Client &cl, int fd, std::map<int, Client> &clients)
 	return true;
 }
 
-bool parse_user_params(const std::string &rest, std::string &username, std::string &realname, int fd, const Client &cl, std::map<int, Client> &clients)
+bool parse_user_params(const std::string &rest,
+						std::string &username,
+						std::string &realname,
+						int fd, const Client &cl,
+						std::map<int, Client> &clients)
 {
 	if (rest.empty()) {
 		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
-					"syntax: USER <username> 0 * :<realname>");
+					 "syntax: USER <username> 0 * :<realname>");
 		return false;
 	}
 
-	username = rest;
-	std::string::size_type sp1 = username.find(' ');
-	if (sp1 != std::string::npos)
-		username.erase(sp1);
+	std::string s = ltrim(rest);
 
+	username = first_token(s);
 	if (username.empty()) {
 		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
-					"Need username: USER <username> 0 * :<realname>");
+					 "Need username: USER <username> 0 * :<realname>");
 		return false;
 	}
+	s = ltrim(s.substr(username.size()));
 
-	std::string::size_type pos_trailing = rest.find(" :");
-	if (pos_trailing == std::string::npos) {
+	std::string mode = first_token(s);
+	if (mode.empty()) {
 		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
-					"Need semicolon before real name: USER <username> 0 * :<realname>");
+					 "Need mode field: USER <username> 0 * :<realname>");
 		return false;
 	}
-	realname = rest.substr(pos_trailing + 2);
+	s = ltrim(s.substr(mode.size()));
 
+	std::string unused = first_token(s);
+	if (unused.empty()) {
+		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
+					 "Need unused field: USER <username> 0 * :<realname>");
+		return false;
+	}
+	s = ltrim(s.substr(unused.size()));
+
+	if (s.empty()) {
+		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
+					 "Need real name: USER <username> 0 * :<realname>");
+		return false;
+	}
+
+	if (s[0] == ':') {
+		realname = s.substr(1);
+	} else {
+		std::string::size_type pos = s.find(" :");
+		if (pos == std::string::npos) {
+			send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
+						 "Need semicolon before real name: USER <username> 0 * :<realname>");
+			return false;
+		}
+		realname = s.substr(pos + 2);
+	}
+
+	if (realname.empty()) {
+		send_numeric(clients, fd, NEED_MORE_PARAMS, cl.nick, "USER",
+					 "Real name cannot be empty");
+		return false;
+	}
 	return true;
 }
 
